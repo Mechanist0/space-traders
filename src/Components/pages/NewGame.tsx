45cf68d2c246f7b318be8621c32de8963fd95125
Melody <cooksleymelody@gmail.com>
2024-09-26 13:48:37 -0500

    get object, broken

--- src/Components/pages/NewGame.tsx
+++ src/Components/pages/NewGame.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { jsx } from "react/jsx-runtime";
 
 function NewGame() {
   const [symbol, setSymbol] = useState("");
-  const [gameInfo, setGameInfo] = useState(null);
+  const [data, setGameInfo] = useState(String);
 
   const registerNewAgent = async () => {
     const options = {
@@ -20,6 +21,7 @@ function NewGame() {
       const response = await fetch("https://api.spacetraders.io/v2/register", options);
       const data = await response.json();
       setGameInfo(data);
+      console.log(data)
     } catch (err) {
       console.error("Error registering agent:", err);
     }
@@ -34,7 +36,8 @@ function NewGame() {
         placeholder="Enter Symbol" />
 
       <button onClick={registerNewAgent}>Register Agent</button>
-      {gameInfo && <h1>{JSON.stringify(gameInfo)}</h1>}
+      <h1>SAVE THIS KEY</h1>
+      {data && <h1>{data.toString()}</h1>}
     </div>
   );
 }
