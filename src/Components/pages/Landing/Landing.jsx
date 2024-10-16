f575858a3f81e5d111a43366841c5e00c7f5d439
BowlBird <bowlbirdcontact@gmail.com>
2024-10-16 18:23:59 -0500

    removed comments

--- src/Components/pages/Landing/Landing.jsx
+++ src/Components/pages/Landing/Landing.jsx
@@ -31,28 +31,4 @@ function Landing() {
   );
 }
 
-// const Landing = () => {
-//   let agent = useLocation().state;
-//   return (
-//     // Divide page into 3 sections
-//     <div className="container-fluid">
-//       <div className="col">
-//         {/* Sidebar */}
-//         <NavSidebar state={agent} />
-
-//         {/* Main content */}
-//         <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-//           <h1>Welcome to Space Traders!</h1>
-//         </main>
-
-//         {/* Top Right Info Panel */}
-//         <ReputationMoney state={agent} />
-
-//         {/* Middle Right Panel */}
-//         <Panel state={agent} />
-//       </div>
-//     </div>
-//   );
-// };
-
 export default Landing;
